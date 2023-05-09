156b6b2609fdc68c73a23392812271dee38714f1
jingrui <790150796@qq.com>
2023-05-09 21:48:54 +0800

    myblog

--- MyBlogClient/src/components/Header/Header.vue
+++ MyBlogClient/src/components/Header/Header.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="header">
-    <div class="navButton">导航按钮</div>
-    <div class="headerLeft">图标</div>
-    <div class="headerMid">搜索</div>
-    <div class="headerRight">头像</div>
+    <div class="navButton">
+        <i class="iconfont fenlei"></i>
+    </div>
+    <div class="headerLeft">
+        <i class="iconfont navicon-wzgl"></i>
+        <span>MyBlog</span>
+    </div>
+<!--    <div class="headerMid">搜索</div>-->
+<!--    <div class="headerRight">头像</div>-->
   </div>
 </template>
 
@@ -15,12 +20,46 @@ export default {
 
 <style>
 .header {
+  display: flex;
+  position: relative;
   width: 100%;
-  height: 60px;
+  height: 50px;
   background-color: #fff;
-  line-height: 60px;
+  align-items: center;
 }
-.header div {
-  float: left;
+/*左侧导航样式*/
+.navButton {
+  display: flex;
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  left: 20px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.fenlei {
+  font-size: 24px;
+}
+
+/*图标样式*/
+.headerLeft {
+  position: absolute;
+  left: 60px;
+  display: flex;
+  width: 140px;
+  height: 40px;
+  line-height: 40px;
+  justify-content: center;
+  align-items: center;
+}
+.navicon-wzgl {
+  font-size: 32px;
+  color: blue;
+  margin-right: 3px;
+}
+.headerLeft > span{
+  font-weight: bolder;
+  font-size: 24px;
 }
 </style>
