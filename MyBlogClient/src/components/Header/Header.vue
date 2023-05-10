04ca77c902fb430e5b1dba061c7d5612ef20d33f
jingrui <790150796@qq.com>
2023-05-10 20:35:30 +0800

    myblog

--- MyBlogClient/src/components/Header/Header.vue
+++ MyBlogClient/src/components/Header/Header.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="header">
-    <div class="navButton">
+    <div class="left">
+      <div class="navButton">
         <i class="iconfont fenlei"></i>
-    </div>
-    <div class="headerLeft">
+      </div>
+      <div class="navIcon">
         <i class="iconfont navicon-wzgl"></i>
         <span>MyBlog</span>
+      </div>
     </div>
-<!--    <div class="headerMid">搜索</div>-->
-<!--    <div class="headerRight">头像</div>-->
+    <div class="mid">
+      <form class="search">
+        <div class="searchLeft">
+          <i class="iconfont sousuo"></i>
+          <input type="search" class="searchContent" placeholder="请输入要搜索的内容" style="outline: none; border-width: 0" >
+        </div>
+        <div class="searchRight">
+          <input type="submit" class="searchIcon" style="outline: none; border-width: 0" value="搜索">
+        </div>
+      </form>
+    </div>
+<!--    <div class="right">头像</div>-->
   </div>
 </template>
 
@@ -19,6 +31,7 @@ export default {
 </script>
 
 <style>
+/*头部背景样式*/
 .header {
   display: flex;
   position: relative;
@@ -27,39 +40,97 @@ export default {
   background-color: #fff;
   align-items: center;
 }
-/*左侧导航样式*/
-.navButton {
+/*左侧样式*/
+.left {
   display: flex;
   position: absolute;
+  width: 20%;
+  height: 50px;
+  left: 20px;
+  align-items: center;
+  cursor: pointer;
+}
+.left .navButton {
+  display: flex;
   width: 40px;
   height: 40px;
-  left: 20px;
+  justify-content: center;
+  align-items: center;
+}
+.left .navButton .fenlei {
+  font-size: 24px;
+}
+.left .navButton .fenlei:hover {
+  transform:rotate(45deg);
+}
+.left .navIcon {
+  display: flex;
+  width: 140px;
+  height: 40px;
   justify-content: center;
   align-items: center;
   cursor: pointer;
 }
-.fenlei {
-  font-size: 24px;
-}
-
-/*图标样式*/
-.headerLeft {
-  position: absolute;
-  left: 60px;
-  display: flex;
-  width: 140px;
-  height: 40px;
-  line-height: 40px;
-  justify-content: center;
-  align-items: center;
-}
-.navicon-wzgl {
+.left .navIcon .navicon-wzgl {
   font-size: 32px;
   color: blue;
   margin-right: 3px;
+  text-shadow:3px 3px 3px pink
 }
-.headerLeft > span{
+.left .navIcon span {
   font-weight: bolder;
   font-size: 24px;
+}
+
+/*中间样式*/
+.mid {
+  display: flex;
+  position: absolute;
+  width: 50%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  left: 300px;
+}
+.mid .search {
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+}
+.mid .search .searchLeft {
+  position: relative;
+  width: 500px;
+  height: 40px;
+  border: 2px solid #e5e4e2;
+  border-right: 0;
+  border-radius: 30px 0 0 30px;
+}
+.mid .search .searchContent {
+  position: absolute;
+  width: 80%;
+  height: 40px;
+  left: 40px;
+}
+.mid .search .sousuo {
+  position: absolute;
+  font-size: 24px;
+  left: 8px;
+  top: 8px;
+  font-weight: bolder;
+}
+.mid .search .searchRight {
+   width: 100px;
+   height: 40px;
+   border: 2px solid #e5e4e2;
+   border-radius: 0 30px 30px 0;
+ }
+.mid .search .searchRight .searchIcon {
+  width: 100%;
+  height: 40px;
+  border-radius: 0 30px 30px 0;
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
