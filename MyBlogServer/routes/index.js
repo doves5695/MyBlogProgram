2a0df7d8bb77eb6af784e3845b939e5c6bd5a71f
jingrui <790150796@qq.com>
2023-05-22 17:22:12 +0800

    myblog

--- MyBlogServer/routes/index.js
+++ MyBlogServer/routes/index.js
@@ -5,6 +5,7 @@ const models = require('../db/models')
 const UserModel = models.getModel('user')
 var svgCaptcha = require('svg-captcha')
 const users = {}
+const sms_until = require('../utils/sms_util')
 
 
 // 密码登录
@@ -58,5 +59,58 @@ router.get('/captcha', function (req, res) {
 });
 
 
+// 发送短信验证码
+router.get('/sendcode', function (req,res) {
+  let phone = req.query.phone;
 
+  let code = sms_until.randomCode(4);
+
+  console.log(`向${phone}发送短信验证码: ${code}`);
+
+  sms_until.sendCode(phone, code, function (success) {
+    if (success) {
+      users[phone] = code
+      console.log('保存验证码:', phone, code)
+      res.send({"code": 0})
+    } else {
+      res.send({"code": 1, msg: '短信验证码发送失败'})
+    }
+  })
+})
+
+
+// 短信登录
+router.post('/login_sms',function (req, res) {
+  var phone = req.body.phone
+  var code = req.body.code
+  console.log('/login_sms', phone, code);
+  if (users[phone] != code) {
+    res.send({code: 1, msg: '手机号或验证码不正确'});
+    return;
+  }
+  // 删除保存的code
+  delete users[phone];
+
+
+  UserModel.findOne({phone}, function (err, user) {
+    if (user) {
+      req.session.userid = user._id
+      res.send({code: 0, data: user})
+    } else {
+      const userModel = new UserModel({phone})
+      userModel.save(function (err, user) {
+        req.session.userid = user._id
+        res.send({code: 0, data: user})
+      })
+    }
+  })
+})
+
+
+// 退出登录
+router.get('/logout',function (req,res) {
+  delete req.session.userid
+
+  res.send({code:0})
+})
 module.exports = router;
